c7837fda16140804a914feb44e15a4fe1b9380ed
gustavo91010 <gustavo910@gmail.com>
2024-03-25 00:24:55 -0300

    imporvments modules

--- src/contex/context.module.ts
+++ src/contex/context.module.ts
@@ -6,10 +6,22 @@ import Provider from 'src/contex/database/entities/provider.entity';
 import { DailyRepository } from './database/repository/daily.repository';
 import { ProviderRepository } from './database/repository/provider.repository';
 import { EmployerRepository } from './database/repository/employer.repository';
+import { TaskRepository } from './database/repository/task.repository';
+import Task from './database/entities/Task.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Daily, Provider, Employer])],
-  providers: [DailyRepository, ProviderRepository, EmployerRepository],
-  exports: [DailyRepository, ProviderRepository, EmployerRepository],
+  imports: [TypeOrmModule.forFeature([Daily, Provider, Employer, Task])],
+  providers: [
+    DailyRepository,
+    ProviderRepository,
+    EmployerRepository,
+    TaskRepository,
+  ],
+  exports: [
+    DailyRepository,
+    ProviderRepository,
+    EmployerRepository,
+    TaskRepository,
+  ],
 })
 export class ContextModule {}
--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import DailyService from './service/daily.service';
 import { ContextModule } from 'src/contex/context.module';
+import EmployerService from './service/employer.service';
+import ProviderService from './service/provider.service';
+import TaskrService from './service/task.service';
 
 @Module({
   imports: [ContextModule],
-  providers: [DailyService],
-  exports: [DailyService],
+  providers: [DailyService, EmployerService, ProviderService, TaskrService],
+  exports: [DailyService, EmployerService, ProviderService, TaskrService],
 })
 export class DomainModule {}
